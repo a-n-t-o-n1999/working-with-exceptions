461f8d4188fe4b1b28bb86e8ca3431e97006709c
user <a_n_t_o_n99@mail.ru>
2026-05-15 13:44:01 +0300

    qqq

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void errorСhecking(String login, String password, String confirmPassword) {
+        if (!login.matches("\\w*") || login.length() > 20) {
+            throw new WrongLoginException();
+        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if (!password.matches("\\w*") || password.length() > 20 || password != confirmPassword) {
+            throw new WrongPasswordException();
         }
     }
+
+
+    public static void main(String[] args) {
+        System.out.println("Проверка");
+        try {
+            errorСhecking("qwe", "qwe", "qwe");
+        } catch (WrongLoginException | WrongPasswordException q) {
+            System.out.println("Что-то пошло не так");
+        }
+    }
+
 }
